1ab9647102bcde8697dc044569b2878900d447e7
kleberga <kleberga@yahoo.com.br>
2021-12-27 22:01:56 -0300

    Project created

---
+++ src/test/java/com/example/webservice_spring_jpa/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice_spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
