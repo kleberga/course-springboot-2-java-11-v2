c64d379ca77a1683029fde159a84284fa6618708
kleberga <kleberga@yahoo.com.br>
2021-12-31 15:16:38 -0300

    Category

---
+++ src/main/java/com/example/webservice_spring_jpa/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.example.webservice_spring_jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.webservice_spring_jpa.entities.Category;
+import com.example.webservice_spring_jpa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
